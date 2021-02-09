5b5861746533d76e43bb2cf102047d8119cc0038
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-02-09 17:26:35 +0530

    Create getRecentAnime.js

---
+++ src/getRecentAnime.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const baseURL = "https://gogoanime.sh"
+const getRecentAnime = async () => {
+  try {
+    let data = await axios({url: baseURL})
+    data = data.data;
+    let recentAnime = []
+
+    let $ = cheerio.load(data);
+    $('ul.items li').each(function (i, elem) {
+      $ = cheerio.load($(this).html())
+      let object = {}
+      object.title = $('p.name a').text() || null
+      object.episode = $('p.episode').text() || null
+      object.image = $('div.img img').attr("src") || null
+      object.link = $('div.img a').attr("href") || null
+      if (object.link) object.link = baseURL + object.link;
+      recentAnime.push(object) 
+    })
+
+    return recentAnime;
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+module.exports = getRecentAnime;
