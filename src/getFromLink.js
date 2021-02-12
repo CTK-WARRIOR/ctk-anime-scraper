a9be2159a115693c8ffa053483952c5ad088279d
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-02-12 16:13:01 +0530

    Update getFromLink.js

--- src/getFromLink.js
+++ src/getFromLink.js
@@ -2,20 +2,29 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 const getFromLink = async (link) => {
-	if(!link) throw "Link not provided";
+  if (!link) throw "Link not provided";
   try {
-  let data = await axios({url: link });
-  let $ = cheerio.load(data.data);
-  let name = $('div.anime_video_body h1').text();
-	let download = $("li.dowloads a").attr("href");
+    let data = await axios({ url: link });
+    let $ = cheerio.load(data.data);
+    let episodeCount = $('ul#episode_page li a.active').attr("ep_end"); 
+    let download = $("li.dowloads a").attr("href");
+    if (!download) throw "Unable to scrap the downlaod link";
+    data = await axios({ url: download });
+    $ = cheerio.load(data.data);
+    download = []
+    let extraLink = []
+    let name = $('span#title').text() || null
+    $("div.dowload").each(function(i, elem) {
+      let object = {}
+      $ = cheerio.load($(this).html())
+      object.quality = $('a').text().replace("Download\n", "").trim()
+      object.link = $('a').attr("href")
+      if (object.quality.startsWith("(")) download.push(object)
+      else extraLink.push(object)
+    })
 
-  if(!download) throw "Unable to scrap the downlaod link";
-  data = await axios({url: download});
-  $ = cheerio.load(data.data);
-  download = $("div.dowload a").attr("href");
-
-  return {name, download}
-  } catch(err) {
+    return { name, episodeCount, download, extraLink }
+  } catch (err) {
     throw err;
   }
 }
