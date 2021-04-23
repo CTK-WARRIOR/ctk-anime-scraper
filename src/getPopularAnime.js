6bd44bccb4bb5984ef7cc93a2e57429d43b94370
Paimon <81248955+PaimonWheeWhee@users.noreply.github.com>
2021-04-23 16:09:58 -0500

    Create getPopularAnime.js

---
+++ src/getPopularAnime.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const baseURL = "https://gogoanime.ai/popular.html"
+const getPopularAnime = async () => {
+  try {
+    let data = await axios({url: baseURL})
+    data = data.data;
+    let popularAnime = []
+
+    let $ = cheerio.load(data);
+    $('ul.items li').each(function (i, elem) {
+      $ = cheerio.load($(this).html())
+      let object = {}
+      object.title = $('p.name a').text() || null
+      object.episode = $('p.episode').text() || null
+      object.image = $('div.img img').attr("src") || null
+      object.link = $('div.img a').attr("href") || null
+      if (object.link) object.link = baseURL + object.link;
+      popularAnime.push(object) 
+    })
+
+    return popularAnime;
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+module.exports = getPopularAnime;
